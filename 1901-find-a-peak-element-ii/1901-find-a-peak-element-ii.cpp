cfd96ce4754286b6ecfc214675c174477cca7c28
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-08-31 03:28:45 +0530

    Time: 4 ms (8.15%), Space: 51 MB (6.03%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
-    int find(vector<int>& nums) {
-        
+    int findMax(vector<int>nums)
+    {
+        int idx=0;
+        int Max=nums[0];
+
+        for(int i=0;i<nums.size();i++)
+        {
+           if(Max<nums[i])
+           {
+                Max=nums[i];
+                idx=i;
+           }
+        }
+
+        return idx;
+
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& nums) {
         if(nums.size()==1)
-        return 0;
+        {
+            int j=findMax(nums[0]);
+            return {0,j};
+        }
 
-        if(nums[0]>nums[1])
-        return 0;
+        int l=findMax(nums[0]);
 
-        if(nums[nums.size()-1]> nums[nums.size()-2])
-        return nums.size()-1;
+        if(nums[0][l]>nums[1][l])
+        {
+            return {0,l};
+        }
+        
+        int n=nums.size();
+        int k=findMax(nums[n-1]);
+
+
+        if(nums[n-1][k]>nums[n-2][k])
+        {
+            return {n-1,k};
+        }
+
+
         int i=1;
         int j=nums.size()-2;
 
@@ -17,11 +48,13 @@ public:
         {
             int mid=(i+j)/2;
 
-            if(nums[mid]>nums[mid-1] && nums[mid+1]<nums[mid])
+            int k=findMax(nums[mid]);
+
+            if(nums[mid-1][k]<nums[mid][k] && nums[mid][k]>nums[mid+1][k])
             {
-                return mid;
+                return {mid,k};
             }
-            else if(nums[mid]<nums[mid+1])
+            else if(nums[mid][k]<nums[mid][k+1])
             {
                 i=mid+1;
             }
@@ -30,52 +63,7 @@ public:
                 j=mid-1;
             }
         }
-        return -1;
-    }
 
-
-
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-      int i=0;
-      int n=mat.size();
-      
-      while(i<n)
-      {
-        int a=find(mat[i]);
-
-            if(i-1>=0 && i+1<n)
-            {
-                if(mat[i-1][a]<mat[i][a] && mat[i+1][a]<mat[i][a])
-                {
-                    return{i,a};
-                }
-            }
-            else if(i-1>=0)
-            {
-                if(mat[i-1][a]<mat[i][a] )
-                {
-                    return{i,a};
-                }
-            }
-            else if(i+1<n)
-            {
-                if(mat[i+1][a]<mat[i][a] )
-                {
-                    return{i,a};
-                }
-            }
-            else
-            {
-                return {i,a};
-            }
-            i++;
-        }
-        
-      
-
-    return {-1, -1};
-
-
-
+        return {-1,-1};
     }
 };
