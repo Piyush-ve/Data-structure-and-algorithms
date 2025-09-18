82ff72728babf5f7d3f69cad8fd8f0ff721dd4a6
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-09-18 11:02:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int rec(string word1,int n, string word2, int m)
+    {
+        if(n==0)
+        {
+            return m;
+        }
+        if(m==0)
+        {
+            return n;
+        }
+        
+        if(word1[n-1]==word2[m-1])
+        {
+            return rec(word1, n-1, word2, m-1);
+        }
+        else
+        {
+            int ans1=rec(word1, n, word2, m-1)+1;
+            int ans2=rec(word1, n-1, word2, m)+1;
+            int ans3=rec(word1, n-1, word2, m-1)+1;
+
+            return min(ans1, min(ans2, ans3));
+        }
+    }
+
+    int tab(string word1, string word2) {
+
+        int n=word1.size();
+        int m=word2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=i;
+        }
+
+        for(int j=0;j<=m;j++)
+        {
+            dp[0][j]=j;
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1];//similar
+                }
+                else
+                {
+                    dp[i][j]=min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;  //delete dp[i-1][j]   // replcae dp[i-1][j-1]  //insert dp[i][j-1]
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    int minDistance(string word1, string word2) {
+
+        int n=word1.size();
+        int m=word2.size();
+
+    //    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+
+      return rec(word1, n, word2, m);
+    }
+};
