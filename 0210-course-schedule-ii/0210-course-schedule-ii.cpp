fade63923e85f8e69823c47307159bcc4e632c9c
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-09-24 06:36:47 +0530

    Time: 43 ms (5.35%), Space: 17.1 MB (99.71%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,3 +1,67 @@
+/*class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>>mp;
+
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+
+            mp[v].push_back(u);
+        }
+
+        vector<int>indeg(numCourses,0);
+
+//calculate indeg
+        for(auto it:mp)
+        {
+            for(auto v:it.second)
+            {
+                indeg[v]++;
+            }
+        }
+
+      queue<int>q;
+
+      for(int i=0;i<numCourses;i++)
+      {
+        if(indeg[i]==0)
+        {
+            q.push(i);
+        }
+      }  
+    vector<bool>vis(numCourses,false);
+    vector<int>ans;
+      while(!q.empty())
+      {
+        int curr= q.front();
+        q.pop();
+        vis[curr]=true;
+
+        ans.push_back(curr);
+
+        for(int v:mp[curr])
+        {
+            indeg[v]--;
+            if(indeg[v]==0)
+            {
+                q.push(v);
+            }
+        }
+      }
+
+      for(int i=0;i<numCourses;i++)
+      {
+        if(vis[i]==false)
+        return {};
+      }
+
+        return ans;
+
+    }
+};
+*/
 class Solution {
 public:
     bool dfsTopological(int src, vector<bool>&vis, vector<bool>&recPath, vector<vector<int>>& graph, stack<int>&s)
