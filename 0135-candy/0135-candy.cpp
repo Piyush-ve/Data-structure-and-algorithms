3e04da7bb712b082048c6d5c8daba1005935b00b
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-09-26 11:07:26 +0530

    Time: 211 ms (5%), Space: 23 MB (27.52%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        if(n<=1)
+        {
+            return n;
+        }
+
+        
+        int ans=0;
+
+        vector<int>candies(n,1);
+
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i-1]<ratings[i])
+            {
+                candies[i]=candies[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i+1]<ratings[i])
+            {
+                candies[i]=max(candies[i+1]+1,candies[i]);
+            }
+        }
+
+
+
+        for(int i=0;i<n;i++)
+        {
+            ans+=candies[i];
+            cout<<candies[i]<<" ";
+        }
+
+        return ans;
+    }
+};
