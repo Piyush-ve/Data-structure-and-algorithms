adc0d21d01bb50362a14a35589dd9157f69ef3ca
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-10-01 05:24:42 +0530

    Time: 27 ms (71.41%), Space: 44.4 MB (14.48%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int rec(string s, int m, string t, int n,vector<vector<int>>&dp)
+    {
+        if(n<0 || (m<0 && n<0))
+        {
+            return 1;
+        }
+        if(m<0)
+        {
+            return 0;
+        }
+
+        if(dp[m][n]!=-1)
+        {
+            return dp[m][n];
+        }
+
+        if(s[m]==t[n])
+        {
+            int pick=rec(s,m-1,t,n-1,dp);
+            int noPick=rec(s,m-1,t,n,dp);
+            return dp[m][n]=pick+noPick;
+        }
+        else
+        {
+            return  dp[m][n]=rec(s,m-1,t,n,dp);
+        }
+
+    }
+
+    int tabulation(string s, int m, string t, int n)
+    {
+        vector<vector<unsigned long long>>dp(m+1, vector<unsigned long long>(n+1,0));
+        dp[0][0]=1;
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=m;i++)
+        {
+            char ch=s[i-1];
+            for(int j=1;j<=n;j++)
+            {
+                char ch2=t[j-1];
+                if(ch==ch2)
+                {
+                    unsigned long long  pick=dp[i-1][j-1];
+                    unsigned long long  noPick=dp[i-1][j];
+                    dp[i][j]=pick+noPick;
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    int numDistinct(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+
+       // vector<vector<int>>dp(m, vector<int>(n,-1));
+       // return rec(s, m-1, t, n-1,dp);
+       return tabulation(s, m, t, n);
+    }
+};
