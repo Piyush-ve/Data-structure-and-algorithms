fa89e427c2c87437ae21713618c202a6d884cdf2
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-11-16 21:51:59 +0530

    Time: 27 ms (83.44%), Space: 14.9 MB (63.77%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int n=s.size();
-        int m=p.size();
 
-        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+    bool isMatch(string s, string p) {
+        int m=s.size();
+        int n=p.size();
+
+        vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+
         dp[0][0]=true;
+
         for(int i=1;i<=n;i++)
         {
-            dp[i][0]=false;
+            if(p[i-1]=='*' && dp[0][i-1]==true)
+            {
+                dp[0][i]=true;
+            }
+
         }
 
         for(int i=1;i<=m;i++)
         {
-            if(p[i-1]=='*')
+            char ch=s[i-1];
+            for(int j=1;j<=n;j++)
             {
-                dp[0][i]=dp[0][i-1];
-            }
-            else
-            {
-                dp[0][i]=false;
-            }
-        }
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==p[j-1] || p[j-1]=='?')
+                char pCh=p[j-1];
+                if(ch==pCh || pCh=='?')
                 {
                     dp[i][j]=dp[i-1][j-1];
                 }
-              
-                else if(p[j-1]=='*')
+                else if(pCh=='*')
                 {
-                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                    dp[i][j]=(dp[i-1][j-1] || dp[i][j-1] ||dp[i-1][j]);
                 }
-                else 
+                else
                 {
                     dp[i][j]=false;
                 }
             }
         }
-
-        return dp[n][m];
+        return dp[m][n];
     }
 };
