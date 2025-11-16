4618ffeec87222376d4953b6e5311790b91e629c
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-11-16 23:05:34 +0530

    Time: 1 ms (63.59%), Space: 9.4 MB (27.67%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,33 +1,53 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+              int n=s.size();
+        int m= p.size();
 
-        dp[0][0] = true;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,false));
 
-        // Handle patterns like a*, a*b*, etc.
-        for (int j = 1; j <= m; j++) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 2];
+        dp[0][0]=true;
+        for(int i=1;i<=m;i++)
+        {
+            if(p[i-1]=='*')
+                dp[0][i]=dp[0][i-2];
+            else
+            {
+                dp[0][i]=false;
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 2];  // zero occurrence
-                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j]; // one or more occurrences
+
+        for(int i=1;i<=n;i++)
+        {
+            char ch=s[i-1];
+            for(int j=1;j<=m;j++)
+            {
+                char pat=p[j-1];
+                if(ch==pat || pat=='.')
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(pat=='*')
+                {
+                    //zero or more preeceding element
+                    dp[i][j]= dp[i][j-2];
+
+                    char prev =p[j-2];
+
+                    if(prev=='.' || prev== ch)
+                    {
+                        dp[i][j]=(dp[i][j]||dp[i-1][j]);
                     }
+                    
+                }
+                else
+                {
+                    dp[i][j]=false;
                 }
             }
         }
-
         return dp[n][m];
+
     }
 };
