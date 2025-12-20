29c2e93909fd5166488d135e57d15fdc740251ec
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2025-12-20 17:34:23 +0530

    Time: 3 ms (91.87%), Space: 12.9 MB (84.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,77 +1,43 @@
 class Solution {
 public:
-    int rec(string word1,int n, string word2, int m,  vector<vector<int>>&dp)
-    {
-        if(n==0)
-        {
-            return m;
-        }
-        if(m==0)
-        {
-            return n;
-        }
-        
-        if(dp[n][m]!=-1)
-        {
-            return dp[n][m];
-        }
-        if(word1[n-1]==word2[m-1])
-        {
-            return dp[n][m]= rec(word1, n-1, word2, m-1,dp);
-        }
-        else
-        {
-            int ans1=rec(word1, n, word2, m-1,dp)+1;
-            int ans2=rec(word1, n-1, word2, m,dp)+1;
-            int ans3=rec(word1, n-1, word2, m-1,dp)+1;
+    int minDistance(string word1, string word2) {
+        int m=word1.size();
+        int n=word2.size();
 
-            return dp[n][m]=min(ans1, min(ans2, ans3));
-        }
-    }
+        vector<vector<int>>dp(m+1, vector<int>(n+1,0));
 
-    int tab(string word1, string word2) {
+        dp[0][0]=0;
 
-        int n=word1.size();
-        int m=word2.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-
-
-        for(int i=0;i<=n;i++)
+        for(int i=1;i<=m;i++)
         {
             dp[i][0]=i;
         }
+        
 
-        for(int j=0;j<=m;j++)
+        for(int j=1;j<=n;j++)
         {
             dp[0][j]=j;
         }
 
-        for(int i=1;i<=n;i++)
+
+
+        for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<=m;j++)
+            char ch=word1[i-1];
+            for(int j=1;j<=n;j++)
             {
-                if(word1[i-1]==word2[j-1])
+                char ch2=word2[j-1];
+                if(ch==ch2)
                 {
-                    dp[i][j]=dp[i-1][j-1];//similar
+                    dp[i][j]=dp[i-1][j-1];
                 }
                 else
                 {
-                    dp[i][j]=min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]))+1;  //delete dp[i-1][j]   // replcae dp[i-1][j-1]  //insert dp[i][j-1]
+                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
                 }
             }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
-
-    int minDistance(string word1, string word2) {
-
-        int n=word1.size();
-        int m=word2.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-
-
-      return rec(word1, n, word2, m, dp);
-    }
+    
 };
