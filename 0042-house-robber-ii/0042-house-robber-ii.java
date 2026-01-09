6d4fa62f5bc45eefcfa85633ac3d0c95074db9f2
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2026-01-10 02:44:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (80.11%) - LeetHub

--- 0042-house-robber-ii/0042-house-robber-ii.java
+++ 0042-house-robber-ii/0042-house-robber-ii.java
@@ -1,27 +1,37 @@
 class Solution {
+    public int helper(int i, int n, int[] nums, int[]dp)
+    {
+        if(i>=n)
+        {
+            return 0;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+       
+            int take= helper(i+2,n,nums,dp)+nums[i];
+            int notTake= helper(i+1,n,nums,dp);
 
-    private int helper(int i, int n, int[] nums, int[] dp) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        int take = nums[i] + helper(i + 2, n, nums, dp);
-        int notTake = helper(i + 1, n, nums, dp);
-
-        return dp[i] = Math.max(take, notTake);
+        return dp[i]=Math.max(take,notTake);
+        
     }
-
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums[0];
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int []dp0=new int[n];
+        Arrays.fill(dp0,-1);
 
-        int[] dp0 = new int[n];
-        int[] dp1 = new int[n];
-        Arrays.fill(dp0, -1);
-        Arrays.fill(dp1, -1);
+        int []dp1=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            dp1[i]=-1;
+        }
 
-        int inclFirst = helper(0, n - 1, nums, dp0);
-        int exclFirst = helper(1, n, nums, dp1);
+        int inclFirst=helper(0,n-1,nums,dp0);
+        int exclFirst=helper(1,n,nums,dp1);
 
-        return Math.max(inclFirst, exclFirst);
+        return Math.max(inclFirst,exclFirst);
+
     }
 }
