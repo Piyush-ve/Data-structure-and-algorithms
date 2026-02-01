a7aac2ee6ed97dd87b615bd830c4264e4eb5f38b
Piyush Verma <153831654+Piyush-ve@users.noreply.github.com>
2026-02-02 00:13:29 +0530

    Time: 24 ms (34.08%), Space: 17.6 MB (23.41%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -30,8 +30,8 @@ public:
                         while(k<l && nums[k]== nums[k-1]) 
                         k++;
 
-                        while(k<l && nums[l]== nums[l+1]) 
-                        l--;
+                       // while(k<l && nums[l]== nums[l+1]) 
+                       // l--;
         
                     }
                     else if(sum>target)
